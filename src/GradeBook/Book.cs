d5c638c6b7124d337ecbeb4a197801083ed94471
Sujeet Kumar <sujeetkumar200075@gmail.com>
2023-10-15 02:56:05 +0530

    - Created class Statistics - Updated ShowStatistics to GetStatistics from all places

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,10 +1,10 @@
 
 namespace GradeBook
 {
-    class Book
+     public class Book
     {
-        List<double> grades;    // 1
-        string name;            // 2
+        private List<double> grades;    
+        private string name;            
         public Book(string name)    // 3
         {
             grades = new List<double>();    // 4
@@ -16,20 +16,24 @@ namespace GradeBook
             this.grades.Add(grade);     // 7
         }
 
-        internal void ShowStatistics()
+        public Statistics GetStatistics()
         {
+            var result = new Statistics();
             var sum = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach(var number in grades){
-                lowGrade = Math.Min(lowGrade, number);
-                highGrade = Math.Max(highGrade, number);
-                sum += number;
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach(var grade in grades)
+            {
+                result.Low = Math.Min(result.Low, grade);
+                result.High = Math.Max(result.High, grade);
+                sum += grade;
             }
-            var avgGrade = sum / grades.Count;
-            Console.WriteLine($"The lowest grade is { lowGrade }");
-            Console.WriteLine($"The highest grade is { highGrade }");
-            Console.WriteLine($"The avreage grade is { avgGrade:N1}");
+            
+            result.Average = sum / grades.Count;
+            
+            return result;
         }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,7 +10,10 @@ namespace GradeBook
             book.AddGrade(89.1);
             book.AddGrade(90.5);
             book.AddGrade(77.5);
-            book.ShowStatistics();
+            var stats = book.GetStatistics();
+            Console.WriteLine($"The lowest grade is { stats.Low }");
+            Console.WriteLine($"The highest grade is { stats.High }");
+            Console.WriteLine($"The avreage grade is { stats.Average:N1}");
         }
     }
 }
