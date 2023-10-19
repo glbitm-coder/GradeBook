65d7c7a7ce85b46aba8ad7cc1953fc6d9845534c
Sujeet Kumar <sujeetkumar200075@gmail.com>
2023-10-20 01:58:47 +0530

    - Creating test file - Tests related to Reference types and values types

---
+++ test/GradeBook.Tests/TypeTests.cs
@@ -0,0 +1,131 @@
+
+namespace GradeBook.Tests;
+
+public class TypeTests
+{
+    [Fact]
+    public void BookCalculatesAnAverageGrade()
+    {
+        var book1 = GetBook("Book 1");
+        var book2 = GetBook("Book 2");
+
+        Assert.Equal("Book 1", book1.Name);
+        Assert.Equal("Book 2", book2.Name);
+        Assert.NotSame(book1, book2);
+    }
+
+    [Fact]
+    public void TwoVarsCanReferenceSameObject()
+    {
+        var book1 = GetBook("Book 1");
+        var book2 = book1;
+
+        Assert.Same(book1, book2);
+        Assert.True(Object.ReferenceEquals(book1, book2));
+    }
+
+    [Fact]
+    public void CanSetNameForReference()
+    {
+        var book1  = GetBook("Book 1");
+        SetName(book1, "New Name");
+
+        Assert.Equal("New Name", book1.Name);
+
+    }
+
+    [Fact]
+    public void CSharpIsPassByValue()
+    {
+        var book1 = GetBook("Book 1");
+        GetBookSetName(book1, "New Name");
+
+        Assert.Equal("Book 1",book1.Name);
+    }
+
+    [Fact]
+    public void CSharpCanPassByRef()
+    {
+        var book1 = GetBook("Book 1");
+        GetBookSetNameForRef(out book1, "New Name");
+
+        Assert.Equal("New Name", book1.Name);
+    }
+
+    [Fact]
+    public void ValueTypesAlsoPassByValue()
+    {
+        var x = GetInt();
+        SetInt(x);
+
+        Assert.Equal(3, x);
+    }
+
+    [Fact]
+    public void StringsBehaveLikeValueTypes()
+    {
+        string name = "Scott";
+        MakeUpperCase(name);
+        Assert.Equal("Scott", name);
+    }
+
+    private void MakeUpperCase(string name)
+    {
+        name.ToUpper();
+    }
+
+    [Fact]
+    public void StringsBehaveLikeRefTypes()
+    {
+        string name = "Scott";
+        var upperCaseName = returnUpperCase(name);
+        Assert.Equal("Scott", name);
+        Assert.Equal("SCOTT", upperCaseName);
+    }
+
+    private string returnUpperCase(string name)
+    {
+        return name.ToUpper();
+    }
+
+    [Fact]
+    public void ValueTypesPassByRef()
+    {
+        var x = GetInt();
+        SetInt(ref x);
+
+        Assert.Equal(42, x);
+    }
+
+    private void SetInt(ref int x)
+    {
+        x = 42;
+    }
+
+    private void SetInt(int x)
+    {
+        x = 42;
+    }
+    private int GetInt()
+    {
+        return 3;
+    }
+
+    private void GetBookSetNameForRef(out Book book, string name)
+    {
+        book = new Book(name);
+    }
+
+    private void GetBookSetName(Book book, string name)
+    {
+        book = new Book(name);
+    }
+    private void SetName(Book book, string name)
+    {
+        book.Name = name;
+    }
+    Book GetBook(string name)
+    {
+        return new Book(name);
+    }
+}
